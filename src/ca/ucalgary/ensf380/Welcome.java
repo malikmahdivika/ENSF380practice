f1235431044e7adae86fb3c9478c5c55da23333a
malikmahdivika <almalik.mahdivika@ucalgary.ca>
2024-07-01 18:38:58 -0600

    message changed

---
+++ src/ca/ucalgary/ensf380/Welcome.java
@@ -0,0 +1,25 @@
+/**
+ * @author Mahdivika AlMalik <a href="mailto:almalik.mahdivika@ucalgary.ca">almalik.mahdivika@ucalgary.ca</a>
+ *
+ * @version	1.4
+ *
+ * @since		1.0
+ */
+
+package ca.ucalgary.ensf380;
+
+/**
+ * 
+ */
+public class Welcome {
+
+	/**
+	 * This prints "Hello, world!" to the terminal window.
+	 * @param args Optional command-line argument
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome back Malik!");
+		System.out.println("Let's get to work shall we?");
+	}
+
+}
